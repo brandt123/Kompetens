f3b8ea256bc73f7426a306dde315968527d17686
brandt123 <pontus.brandt@gmail.com>
2015-03-18 19:23:44 +0100

    Bytt repository

---
+++ Kompetens/Controllers/MovieController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Kompetens.Models;
+
+namespace Kompetens.Controllers
+{
+    [RoutePrefix("searchcontroller")]
+    public class MovieController : ApiController
+    {
+        [HttpGet]
+        [Route("")]
+        public IHttpActionResult SearchMovie(string q)
+        {
+
+            var data = WebApiConfig.GraphClient.Cypher
+               .Match("(m:Movie)")
+               .Where("m.title =~ {title}")
+               .WithParam("title", "(?i).*" + q + ".*")
+               .Return<Movie>("m")
+               .Results.ToList();
+
+            var data2 = WebApiConfig.GraphClient.Cypher
+                .Match("(a)-[r]-(b)")
+                .Where("a.title =~ {title}")
+                .WithParam("title", "(?i).*" + q + ".*")
+                .ReturnDistinct(r => new { Type = r.Type() })
+                .Results;
+
+            return Ok(data.Select(c => new { movie = c }));
+        }
+    }
+}
---
+++ Kompetens/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Kompetens.MvcApplication" Language="C#" %>
