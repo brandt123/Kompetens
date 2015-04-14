0ab3bc73ec7f555208e20e358bd6bbf7f2c15f0c
Pontus Brandt <pontus.brandt@gmail.com>
2015-04-14 13:46:16 +0200

    Lägg till projekt

--- Kompetens/Controllers/HomeController.cs
+++ Kompetens/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Kompetens.ViewModels;
 
 namespace Kompetens.Controllers
 {
@@ -16,13 +17,40 @@ namespace Kompetens.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
+            var vm = new RegisterViewModel();
 
-            return View();
+            return View(vm);
         }
 
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public class User
+        {
+            public string Name { get; set; }
+            public string Problem { get; set; }
+            public string Solution { get; set; }
+            public string Result { get; set; }
+        }
+
+        [HttpPost]
+        public ActionResult About(RegisterViewModel vm) {
+            Response.Write("DET FUNKAR");
+                
+            var newUser = new User { Name = vm.Name, Problem = vm.Problem, Solution = vm.Solution, Result = vm.Result };
+            WebApiConfig.GraphClient.Cypher
+                .Merge("(user:User { Name: {Name} })")
+                .Set("user = {newUser}")
+                .WithParams(new
+                {
+                    Name = newUser.Name,
+                    newUser
+                })
+                .ExecuteWithoutResults();
 
             return View();
         }
---
+++ Kompetens/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kompetens.ViewModels
+{
+    public class RegisterViewModel
+    {
+
+        public string Name { get; set; }
+
+        public string Problem { get; set; }
+
+        public string Solution { get; set; }
+
+        public string Result { get; set; }
+    }
+}
